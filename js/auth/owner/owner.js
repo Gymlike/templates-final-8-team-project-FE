3891ef552f2f4d9774b3440a09ef29d683957c0f
khakyy <116627270+khakyy@users.noreply.github.com>
2023-03-09 15:58:50 +0900

    이메일 인증 관련 수정

--- js/auth/owner/owner.js
+++ js/auth/owner/owner.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function ($) {
 });
 
 document.querySelector('.img__btn').addEventListener('click', function () {
-document.querySelector('.cont').classList.toggle('s--signup');
+    document.querySelector('.cont').classList.toggle('s--signup');
 });
 
 
@@ -70,6 +70,10 @@ function signUp() {
             document.querySelector('.cont').classList.toggle('s--signup');
             location.reload();
         }).fail(function (response) {
+            console.log(response)
+            if (response.status === 400) {
+                alert(response.responseText)
+            }
             console.log(response.responseJSON);
             if (response.responseJSON.statusCode === 409) {
                 alert("중복된 ID입니다.")
@@ -177,50 +181,58 @@ function getOwnerMe() {
 
 //이메일 보내기
 function sendEmail() {
-    var settings = {
-        "url": "http://ec2-3-36-89-51.ap-northeast-2.compute.amazonaws.com/api/user/email?email=" + $('#email').val(),
-        "method": "POST",
-        "timeout": 0,
-      };
-      
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        alert("이메일이 발송되었습니다.")
-        $('#verifyEmail').show();
-        $('#verifyEmail2').show();
-        $('#sendEmail2').hide();
-        $('#sendEmail3').show();
-      }).fail (function (response) {
-        console.log(response.responseJSON);
-        if (response.responseJSON.status === 404) {
-            alert("중복된 이메일입니다. 다른 이메일을 사용하여주세요.")
-        }
-        if (response.responseJSON.statusCode === 400) {
-            alert("잘못된 이메일형식입니다. 다시 한번 확인해주세요.")
-        }
-    })}
+	var settings = {
+		"url": "http://localhost:8080/api/user/email?email=" + $('#email').val(),
+		"method": "POST",
+		"timeout": 0,
+	};
 
-    //이메일 코드 인증
-    function verifymail() {
-        var settings = {
-            "url": "http://ec2-3-36-89-51.ap-northeast-2.compute.amazonaws.com/api/user/verifyCode?code=" + $('#EmailCode').val(),
-            "method": "POST",
-            "timeout": 0,
-          };
-          
-          $.ajax(settings).done(function (response) {
-            if(response == 1) {
-                localStorage.setItem('verifyEmailCode', 1)
-                alert("이메일 인증에 성공하였습니다.")
-                $('#emailOk').show();
-                $('#verifyEmail').hide();
-                $('#verifyEmail2').hide();
-                $('#sendEmail3').hide();
-                email.readOnly = true;
-            } else {
-                localStorage.setItem('verifyEmailCode', 0)
-                console.log(response);
-                alert("전송된 코드를 다시 한번 확인해주세요.")
-            }
-          });
-        }
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		alert("이메일이 발송되었습니다.")
+		$('#verifyEmail').show();
+		$('#verifyEmail2').show();
+		$('#sendEmail2').hide();
+		$('#sendEmail3').show();
+	}).fail(function (response) {
+		console.log(response.responseJSON);
+		if (response.responseJSON.status === 404) {
+			alert("중복된 이메일입니다. 다른 이메일을 사용하여주세요.")
+		}
+		if (response.responseJSON.statusCode === 400) {
+			alert("잘못된 이메일형식입니다. 다시 한번 확인해주세요.")
+		}
+	})
+}
+
+//이메일 코드 인증
+function verifymail() {
+	var settings = {
+		"url": "http://localhost:8080/api/user/verifyCode?code=" + $('#EmailCode').val() + "&email=" + $('#email').val(),
+		"method": "POST",
+		"timeout": 0,
+	};
+
+	$.ajax(settings).done(function (response) {
+		if (response == 1) {
+			localStorage.setItem('verifyEmailCode', 1)
+			alert("이메일 인증에 성공하였습니다.")
+			$('#emailOk').show();
+			$('#verifyEmail').hide();
+			$('#verifyEmail2').hide();
+			$('#sendEmail3').hide();
+			email.readOnly = true;
+		} else {
+			localStorage.setItem('verifyEmailCode', 0)
+			console.log(response);
+			alert("이메일 인증 후 다시 시도해주세요.")
+		}
+	}).fail(function (response) {
+		console.log(response.responseJSON);
+		if (response.responseJSON.statusCode === 500) {
+			alert("인증에 실패하였습니다. 인증 코드를 확인해주세요")
+		} if (response.responseJSON.statusCode === 501) {
+			alert("만료된 인증 코드입니다. 재발송 후 다시 시도해주세요.")
+		}
+	})
+}
