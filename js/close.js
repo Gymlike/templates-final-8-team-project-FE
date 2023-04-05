ce828df470814496e7febdf0b5af5a4f2ba6122c
allnight5 <45612782+allnight5@users.noreply.github.com>
2023-04-05 19:19:22 +0900

    Update close.js

--- js/close.js
+++ js/close.js
@@ -1,4 +1,15 @@
 window.addEventListener('beforeunload', function(event){
+    
+    
+    //확인하고 싶다면 아래와 같이 두줄이나
+  //event.preventDefault();만 넣어주면 됩니다. 모든 브라우저가 안될 수 도 있으니 두번째것을
+  //넣어주는 것이 좋습니다.
+    // 명세에 따라 preventDefault는 호출해야하며, 기본 동작을 방지합니다.
+    event.preventDefault();
+    
+    // 대표적으로 Chrome에서는 returnValue 설정이 필요합니다.
+    event.returnValue = '';
+    
     //로그아웃
     function logout() {
         var settings = {
@@ -24,12 +35,4 @@ window.addEventListener('beforeunload', function(event){
             $('#loginUser').clear(response.nickName + "님 환영합니다.");
         });
     }
-    //확인하고 싶다면 아래와 같이 두줄이나
-  //event.preventDefault();만 넣어주면 됩니다. 모든 브라우저가 안될 수 도 있으니 두번째것을
-  //넣어주는 것이 좋습니다.
-    // 명세에 따라 preventDefault는 호출해야하며, 기본 동작을 방지합니다.
-    //event.preventDefault();
-    
-    // 대표적으로 Chrome에서는 returnValue 설정이 필요합니다.
-    //event.returnValue = '';
 });
